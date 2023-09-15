91124458cf0cc3be124c4876b9f2f7966d7d4d78
MarcCodes456 <64700917+MarcCodes456@users.noreply.github.com>
2023-09-15 14:16:29 -0400

    Update UDPReceive.cs

--- UDPReceive./UDPReceive.cs
+++ UDPReceive./UDPReceive.cs
@@ -6,6 +6,38 @@ using System.Net.Sockets;
 using System.Threading;
 
 public class UDPReceive: MonoBehaviour{
-//19:19 In vid
+  Thread receiveThread;
+  UdpClient client;
+  public int port = 5052;
+  public bool printToConsole = false;
+  public string data;
 
+  public void Start(){
+    receiveThread = new Thread(
+      new ThreadStart(ReceiveData));
+    receiveThread.IsBackground = true;
+    receiveThread.Start();
+  }
+
+  //Actual Receiving The Thread and stuff
+  private void RecevieData(){
+    client = new UdpClient(port);
+    while (startRecieving){
+
+      try{
+        IPEndPoint anyIP = new IPEndPoint(IP Address.Any, 0);
+        byte[] dataByte = client.Recevie(ref anyIP);
+        data = Encoding.UTF8.GetString(dataByte);
+
+        if(printToConsole){print(data);}
+        }
+        catch (Exception err)
+        {
+          print(err.ToString())
+        }
+      }
+    }
+  }
+  
+  //19:19 In vid
 }
