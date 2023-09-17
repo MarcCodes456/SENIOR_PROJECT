e9081c67d7f7fd16399a2f4921d216c213bbb2c6
MarcCodes456 <64700917+MarcCodes456@users.noreply.github.com>
2023-09-17 18:10:06 -0400

    Fixed/Updated Main.py
    This is after I imported mediapipe and Numpy(Good Progress so far everything work GGez)
    
    On line 19: "img =" was NOT added messing up the whole thing FIXED NOW THO

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
-Import cv2
+import cv2
 #https://www.youtube.com/watch?v=RQ-2JWzNc6k Move Code Over(2:42) And dowload stuff
-from cvzone.HandTrackingModule import HandDetector
+from HandTrackingModule import HandDetector
+import numpy as np
+import mediapipe as mp
 # Parameters
 width, height = 1280, 720
 
@@ -14,13 +16,14 @@ detector = HandDetector(maxHands=1, detectionCon=0.8)
 
 while True:
     success, img = cap.read()
-    hands, detector.findHands(img)
+    hands, img = detector.findHands(img)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
     data = []
 
 
     # Landmark Values - (x,y,z) * 21
+    
     if hands:
         hand = hands[0]
         lmList = hand['lmList']
@@ -28,6 +31,6 @@ while True:
         for lm in lmList:
             data.extend([lm[0], height - lm[1], lm[2]])
         print(data)
-
+    
 ## [6:42 Install Mediapip]
 # [14:39 Sending Data to Unity]
