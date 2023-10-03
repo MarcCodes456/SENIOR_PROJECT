4f879c4eef1aeab80c439070df36424579c1dc9d
MarcCodes456 <64700917+MarcCodes456@users.noreply.github.com>
2023-10-03 13:54:53 -0400

    Main.py [Imported Socket, Unity Data]
    READ TO SEND TO UNITY GG EZ

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 import cv2
-#https://www.youtube.com/watch?v=RQ-2JWzNc6k Move Code Over(2:42) And dowload stuff
 from HandTrackingModule import HandDetector
 import numpy as np
 import mediapipe as mp
+import socket
+
+
 # Parameters
 width, height = 1280, 720
 
@@ -14,11 +16,17 @@ cap.set(4, height)
 # Hand Detector
 detector = HandDetector(maxHands=1, detectionCon=0.8)
 
+#Communication To Unity
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverAddressPort = ("127.0.0.1",6969) #5052 alt port
+
 while True:
     success, img = cap.read()
     hands, img = detector.findHands(img)
-    cv2.imshow("Image", img)
+    cv2.imshow("Keaton Oppenheimer", img)
+    
     cv2.waitKey(1)
+    
     data = []
 
 
@@ -27,10 +35,10 @@ while True:
     if hands:
         hand = hands[0]
         lmList = hand['lmList']
-        print(lmList)
+        #print(lmList)
         for lm in lmList:
             data.extend([lm[0], height - lm[1], lm[2]])
-        print(data)
-    
-## [6:42 Install Mediapip]
-# [14:39 Sending Data to Unity]
+        #print(data)
+        sock.sendto(str.encode(str(data)), serverAddressPort)
+
+# 10/3 Create Unity Project and Send Data their
