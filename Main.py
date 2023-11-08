d2f3db15a5b51259246bd4fa5ee263aee9c62f34
MarcCodes456 <64700917+MarcCodes456@users.noreply.github.com>
2023-11-08 13:28:41 -0500

    Main.py Left & Right Dect.WIP

--- Main.py
+++ Main.py
@@ -36,20 +36,35 @@ while True:
     if hands:
         hand1 = hands[0]
         lmList = hand1['lmList']
+        handType1 = hand1["type"]
         print(lmList)
         for lm in lmList:
             data.extend([lm[0], height - lm[1], lm[2]])
         print(data)
+        if handType1 == "Right":
+            for ht in handType1:
+                print(handType1)
+        
         sock.sendto(str.encode(str(data)), serverAddressPort)
+        sock.sendto(str.encode(str(handType1)), serverAddressPort)
 
-    #second hand
-    if hands:
-        hand2 = hands[0]
-        lmList2 = hand2['lmList']
-        print(lmList2)
-        for lm in lmList2:
-            data.extend([lm[0], height - lm[1], lm[2]])
-        print(data)
-        sock.sendto(str.encode(str(data)), serverAddressPort)
+        #second hand
+        if len(hands) == 2:
+            hand2 = hands[0]
+            lmList2 = hand2['lmList']
+            handType2 = hand2["type"]
+            print(lmList2)
+            for lm in lmList2:
+                data.extend([lm[0], height - lm[1], lm[2]])
+            print(data)
+            if handType2 == "Left":
+                for ht in handType2:
+                    print(handType2)
+
+            sock.sendto(str.encode(str(data)), serverAddressPort)
+            sock.sendto(str.encode(str(handType2)), serverAddressPort)
+        print(" ")  # New line for better readability of the printed output
+
+    
 
 # 10/3 Create Unity Project and Send Data their
