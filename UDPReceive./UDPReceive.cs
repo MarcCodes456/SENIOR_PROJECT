95dd20c96fa2548ada7131bbe2ae8052048be302
MarcCodes456 <64700917+MarcCodes456@users.noreply.github.com>
2023-11-09 13:45:44 -0500

    UDPReceive Seperate Ports Created

--- UDPReceive./UDPReceive.cs
+++ UDPReceive./UDPReceive.cs
@@ -8,12 +8,13 @@ using System.Threading;
 public class UDPReceive: MonoBehaviour{
   Thread receiveThread;
   UdpClient client;
+  UdpClient clientAlt;
   public int port = 5052;
+  public int portAlt = 4200;
   public bool startRecieving = true;
   public bool printToConsole = false;
   public string data;
   public string handType;
-
   
 
   public void Start(){
@@ -26,14 +27,22 @@ public class UDPReceive: MonoBehaviour{
   //Actual Receiving The Thread and stuff
   private void ReceiveData(){
     client = new UdpClient(port);
+    clientAlt = new UdpClient(portAlt);
     while (startRecieving){
 
       try{
         IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+        //Pretty much just duplicated these
         byte[] dataByte = client.Receive(ref anyIP);
-        data = Encoding.UTF8.GetString(dataByte);
+        byte[] handByte = clientAlt.Receive(ref anyIP);
 
-        if(printToConsole){print(data);}
+        data = Encoding.UTF8.GetString(dataByte);
+        handType = Encoding.UTF8.GetString(handByte);
+        if(printToConsole)
+        {
+          print(data);
+          print(handType);
+        } 
         }
         catch (Exception err)
         {
